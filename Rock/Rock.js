5e47797b259ce9ea97d6ca15e03bc2bd4e2cd82a
coolasfk <fk.przybyla@gmail.com>
2022-07-14 14:34:28 +0200

    commit

--- Rock/Rock.js
+++ Rock/Rock.js
@@ -8,43 +8,42 @@ const list = ["rock", "paper", "scissors"];
 
 button.addEventListener('click', func);
 
-
-
-
-
-
 function func() {
     var item = document.getElementById('you').value;
     you.innerHTML = item;
 
-    if (item == ''){
-        result.innerHTML = "wrong input";
-        return false;
-    } 
-
+ 
     if (item !== 'paper' && item !== 'scissors' && item !== 'rock'){
         result.innerHTML = "wrong input";
+        result.style.color = "magenta";
         return false;
     } 
 
-
+  
     var item2 = list[Math.floor(Math.random() * list.length)];
     oponnent.innerHTML = item2;
 
     if(item == "scissors" && item2 == "scissors") {
         result.innerHTML = 'Try again!'
+        result.style.color = "white";
     } else if (item == "rock" && item2 == "rock") {
         result.innerHTML = 'Try again!'
+        result.style.color = "white";
     } else if (item == "paper" && item2 == "paper") {
         result.innerHTML = 'Try again!'
+        result.style.color = "white";
     } else if (item == "rock" && item2 == "scissors") {
         result.innerHTML = 'Congrats! You WON!'
+        result.style.color = "white";
     } else if (item == "scissors" && item2 == "paper") {
         result.innerHTML = 'Congrats! You WON!'
+        result.style.color = "white";
     } else if (item == "paper" && item2 == "rock") {
         result.innerHTML = 'Congrats! You WON!'
+        result.style.color = "white";
     } else {
         result.innerHTML = 'Computer beat you up, you loser!'
+        result.style.color = "white";
     }
 
 }
